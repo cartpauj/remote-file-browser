dde10fd51870dce3f2129aaa5319becd620836fc
cartpauj <cartpauj@caseproof.com>
2025-06-18 17:33:08 -0600

    more cleaning

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+  target: 'node',
+  mode: 'development', // Change from 'none' to 'development' to reduce aggressive optimizations
+  entry: './src/extension.ts',
+  output: {
+    path: path.resolve(__dirname, 'out'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2'
+  },
+  externals: {
+    vscode: 'commonjs vscode'
+  },
+  node: {
+    __dirname: false
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  },
+  devtool: 'source-map',
+  optimization: {
+    minimize: false, // Disable minification that might break crypto operations
+    mangleExports: false,
+    usedExports: false
+  },
+  infrastructureLogging: {
+    level: "log",
+  },
+};
